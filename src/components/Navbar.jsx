28b1829eda625b0ba0097e7a8a593da705897a2c
Sophie Perks <sophieperks@gmail.com>
2023-04-05 11:53:22 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/art">Art</Link>
+        </li>
+        <li>
+          <Link to="/plate-shop">Plate Shop</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
