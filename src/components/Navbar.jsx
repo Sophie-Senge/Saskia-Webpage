61bfc9b28e542f1815d779168cd66bb4b2768551
Sophie Perks <sophieperks@gmail.com>
2023-04-05 16:43:26 +0100

    hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 function Navbar() {
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
+
     const links = [
         {
             id: 1,
@@ -30,25 +34,40 @@ function Navbar() {
 
     ]
     return (
-        <nav className="nav flex justify-between text-white items-center px-4 fixed w-full h-20 bg-slate-600">
+        <nav className="nav flex justify-around text-white items-center px-4 fixed w-full h-20 bg-slate-600">
             <div >
-               <h1 className="text-5xl px-4 uppercase font-headings hover:text-gray-200">
-               <NavLink to="/">saskia spender</NavLink>
+                <h1 className="text-5xl tracking-wider font-semibold px-4 uppercase font-headings hover:text-gray-200">
+                    <NavLink to="/">saskia spender</NavLink>
                 </h1>
             </div>
-            <ul className="hidden lg:flex capitalize font-headings">
-                {links.map(({id, link, name}) => (
+            <ul className="hidden lg:flex uppercase tracking-wide text-sm font-headings">
+                {links.map(({ id, link, name }) => (
                     <li key={id} className="px-4 py-4 hover:scale-105 duration-200 hover:text-gray-200">
-                    <NavLink to={link}>{name}</NavLink>
-                </li>
+                        <NavLink to={link}>{name}</NavLink>
+                    </li>
 
                 ))}
-
-
-
-                
-               
             </ul>
+
+            <div
+        onClick={handleClick}
+        className='cursor-pointer text-white z-10 lg:hidden'>
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+
+      </div>
+      {nav && (
+        <ul className="uppercase tracking-wide flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-slate-500 text-sm font-headings">
+        {links.map(({ id, link, name }) => (
+            <li key={id} className="px-4 py-4 hover:scale-105 duration-200 hover:text-gray-200">
+                <NavLink to={link}>{name}</NavLink>
+            </li>
+
+        ))}
+    </ul>
+        
+      )}
+
+         
 
         </nav>
     );
