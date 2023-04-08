a21d6a732937dbd55aa5edc9a11cd320a2402579
Sophie Perks <sophieperks@gmail.com>
2023-04-08 13:36:48 +0100

    vase page added

--- src/pages/Vases/Vases.jsx
+++ src/pages/Vases/Vases.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import vaseList from './vaseData';
 
 function Vases (){
     return(
-        <div className='bg-slate-500 h-screen w-full'>
-            <p>Vases</p>
+        <div className="w-full h-max bg-white">
+          <div className=" grid md:grid-rows-2 md:grid-cols-3 gap-8 grid-rows-1 justify-center items-center  text-center mb-28 mx-28 pt-40 ">
+            {vaseList.map(({id, title, image, description }) =>{
+                return(
+          
+                <div key={id} className="group overflow-hidden">
+                <img src={image} alt={title} className="h-80 w-72 object-cover"/>
+                <div className="">
+                <h1 className="text-xl text-zinc-900 font-headings">{title}</h1>
+                <p className='text-sm'>{description}</p>
+                </div>
+            </div> 
+            
+                )
+                
+            })}
+           
+           </div>
         </div>
     )
 }
---
+++ src/pages/Vases/vaseData.jsx
@@ -0,0 +1,96 @@
+const vaseList = [
+    {
+        id: 1,
+        title: "Title",
+        image: require("./assets/100-9250.JPG"),
+        className: " absolute -my-44 mx-[94px] md:mx-[110px] opacity-0 group-hover:opacity-100",
+        page: '/vases',
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 2,
+        title: "Title",
+        image: require("./assets/100-9519.JPG"),
+        className: "absolute -my-44 mx-20 md:mx-24 opacity-0 group-hover:opacity-100",
+        page: '/baskets',
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 3,
+        title: "Title",
+        image: require("./assets/100-9557.JPG"),
+        className: " absolute -my-44 mx-[96px] md:mx-[110px] opacity-0 group-hover:opacity-100",
+        page: '/bowls',
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 4,
+        title: "Title",
+        image: require("./assets/100-9604.JPG"),
+        className: " absolute -my-44 mx-[52px] md:mx-[60px] opacity-0 group-hover:opacity-100",
+        page: '/dreamscapes',
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 5,
+        title: "Title",
+        image: require("./assets/100-9686.JPG"),
+        className: "absolute -my-44 mx-24 md:mx-28 opacity-0 group-hover:opacity-100",
+        page: '/walls',
+        description: 'Here will be description and measurements'
+        
+
+    },
+     {
+        id: 6,
+        title: "Title",
+        image: require("./assets/100-9697.JPG"),
+        className: "absolute -my-44 mx-16 md:mx-20 opacity-0 group-hover:opacity-100",
+        page: '/sculptures',
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 7,
+        title: "Title",
+        image: require("./assets/100-9727.JPG"),
+        className: "absolute -my-44 mx-16 md:mx-20 opacity-0 group-hover:opacity-100",
+        page: '/sculptures',
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 8,
+        title: "Title",
+        image: require("./assets/100-9734.JPG"),
+        className: "absolute -my-44 mx-16 md:mx-20 opacity-0 group-hover:opacity-100",
+        page: '/sculptures',
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 9,
+        title: "Title",
+        image: require("./assets/100-9784.JPG"),
+        className: "absolute -my-44 mx-16 md:mx-20 opacity-0 group-hover:opacity-100",
+        page: '/sculptures',
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 10,
+        title: "Title",
+        image: require("./assets/100-9847.JPG"),
+        className: "absolute -my-44 mx-16 md:mx-20 opacity-0 group-hover:opacity-100",
+        page: '/sculptures',
+        description: 'Here will be description and measurements'
+
+    },
+
+]
+
+export default vaseList;
