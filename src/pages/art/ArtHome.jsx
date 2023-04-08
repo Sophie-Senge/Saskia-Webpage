a6d02ce219dba5170985d24f1fe7fcc71eec3641
Sophie Perks <sophieperks@gmail.com>
2023-04-08 12:20:18 +0100

    zoom css

--- src/pages/art/ArtHome.jsx
+++ src/pages/art/ArtHome.jsx
@@ -5,11 +5,12 @@ import { NavLink } from 'react-router-dom';
 function ArtHome (){
     return(
         <div className="w-full h-max bg-white">
-          <div className=" grid md:grid-rows-2 md:grid-cols-3 gap-6 grid-rows-1 justify-center items-center text-center mb-28 mx-28 pt-40 ">
+          <div className=" grid md:grid-rows-2 md:grid-cols-3 gap-6 grid-rows-1 justify-center items-center  text-center mb-28 mx-28 pt-40 ">
             {artList.map(({id, title, className, image, page}) =>{
                 return(
-           <NavLink to={page}><div key={id} className="group hover:scale-105 duration-200">
-                <img src={image} alt={title} className="h-80 w-72 object-cover hover:grayscale"/>
+           <NavLink to={page}>
+                <div key={id} className="group overflow-hidden">
+                <img src={image} alt={title} className="h-80 w-72 object-cover duration-500  hover:scale-125 hover:grayscale"/>
                 <div className={className}>
                 <h1 className="text-2xl text-zinc-100 font-semibold font-headings">{title}</h1>
                 </div>
