fb323a094ad66701dfe64bd5e845688bca86719b
Sophie Perks <sophieperks@gmail.com>
2023-04-11 11:59:24 +0100

    contact page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { SiInstagram } from "react-icons/si";
 
 function Footer (){
     return(
         <div className="w-full flex bg-white fixed bottom-0">
         <div className=" mx-auto flex flex-row justify-center text-center py-4">
+        <a className='mt-4 px-2' href="https://www.instagram.com/saskiaspender/?hl=en"><SiInstagram size={20}/></a>
        
            <a href="https://leliascarfiotti.com/"> <p className="mt-4 px-2 text-xs font-headings leading-6  text-black">
             photo©2020 lelia scarfiotto
@@ -17,4 +19,4 @@ function Footer (){
     )
 }
 
-export default Footer
+export default Footer;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import profile from '../assets/images/SaskiaContact.webp'
+import profile from '../assets/images/SaskiaContact.webp';
+import { SiInstagram } from "react-icons/si";
+
 
 function Contact() {
   return (
-    <div id="contact" className="w-full h-screen text-center grid md:grid-rows-1 md:grid-cols-2 gap-8 grid-rows-1 pt-24">
-      <div className='object-cover p-8'>
-        <img src={profile} alt="saskia spender" />
+    <div id="contact" className="mx-auto h-screen text-center place-content-center grid md:grid-cols-2 grid-rows-1 pt-32">
+      <div className='pl-10'>
+        <img className='object-cover h-[90%]' src={profile} alt="saskia spender" />
       </div>
-      <div className='justify-center mx-auto flex flex-col w-full h-full max-w-screen-lg'>
-        <div className='pb-8'>
-         
+      <div className='pr-20'>
+        <h2 className="text-2xl font-semibold uppercase tracking-widest pb-2 font-headings">Contact</h2>
+        <p>saskia@saskiaspender.com</p>
+        <a className='grid place-content-center p-2' href="https://www.instagram.com/saskiaspender/?hl=en"><SiInstagram size={20}/></a>
 
-          <h2  className="pt-6 text-2xl text-zinc-400 hover:text-indigo-700">Send me a message!</h2>
-          <p  className='text-zinc-400 hover:text-indigo-700'>or email me directly at sophieperks@gmail.com</p>
-        </div>
-        <div className='flex justify-center items-center'>
-          <form action='https://getform.io/f/b4e5ea89-cb07-46db-b030-021cf444088b' method='POST' className='flex flex-col  md:w-1/2'>
-            <input  type="text" name='name' placeholder='Name' className='p-2 bg-transparent text-zinc-300 border-yellow-500 border-b-2 focus:outline-none' />
-            <input  type="text" name='email' placeholder='Email' className='my-4 p-2 border-b-2 border-yellow-500 bg-transparent text-zinc-300 focus:outline-none' />
-            <textarea  name="message" rows="7" placeholder='Enter your message' className='bg-transparent border-b-2 border-yellow-500 p-2 text-zinc-300 focus:outline-none'></textarea>
-            <button className=''  type="submit"> Send
+        <div className=''>
+          <form action='https://getform.io/f/b4e5ea89-cb07-46db-b030-021cf444088b' method='POST' className='flex flex-col'>
+            <input type="text" name='name' placeholder='Name' className='p-2 border-zinc-300 border-b-[1px] focus:outline-none' />
+            <input type="text" name='email' placeholder='Email' className='my-4 p-2 border-zinc-300 border-b-[1px] focus:outline-none' />
+            <textarea name="message" rows="7" placeholder='Enter your message' className=' border-zinc-300 border-b-[1px] p-2 focus:outline-none'></textarea>
+            <button className='border border-spacing-2' type="submit"> Send
             </button>
           </form>
 
