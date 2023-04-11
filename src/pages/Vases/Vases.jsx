5fe315b2df007d631bac22f16673ec3e5e15c2f5
Sophie Perks <sophieperks@gmail.com>
2023-04-11 17:09:38 +0100

    added aos

--- src/pages/Vases/Vases.jsx
+++ src/pages/Vases/Vases.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useEffect} from "react";
 import vaseList from './vaseData';
+import Aos from "aos";
+import "aos/dist/aos.css"; 
 
 function Vases (){
+    useEffect(() =>{
+        Aos.init({duration: 3000});
+      }, []);
     return(
         <div className="w-full h-max bg-white">
           <div className=" grid md:grid-rows-2 md:grid-cols-3 gap-10 grid-rows-1 justify-center items-center  text-center mb-28 mx-28 pt-40 ">
             {vaseList.map(({id, title, image, description }) =>{
                 return(
           
-                <div key={id} className="group overflow-hidden">
+                <div data-aos="fade-up" key={id} className="group overflow-hidden">
                 <img src={image} alt={title} className="h-80 w-72 object-cover"/>
                 <div className="">
                 <h1 className="text-xl text-zinc-900 font-headings">{title}</h1>
--- src/pages/art/ArtHome.jsx
+++ src/pages/art/ArtHome.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 import artList from './ArtData';
 import { NavLink } from 'react-router-dom';
 
+
 function ArtHome (){
+  
     return(
         <div className="w-full h-max bg-white">
-          <div className=" grid md:grid-rows-2 md:grid-cols-3 gap-6 grid-rows-1 justify-center items-center  text-center mb-28 mx-28 pt-40 ">
+          <div  className="grid md:grid-rows-2 md:grid-cols-3 gap-10 grid-rows-1 justify-center items-center  text-center mb-28 mx-28 pt-40 ">
             {artList.map(({id, title, className, image, page}) =>{
                 return(
            <NavLink to={page}>
@@ -14,7 +16,8 @@ function ArtHome (){
                 <div className={className}>
                 <h1 className="text-2xl text-zinc-100 font-semibold font-headings">{title}</h1>
                 </div>
-            </div> </NavLink> 
+            </div> 
+            </NavLink> 
             
                 )
                 
