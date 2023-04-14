df148d170b5d672c9ae4d59586e336a9bfb1c0f3
Sophie Perks <sophieperks@gmail.com>
2023-04-14 17:40:26 +0100

    fixed error

--- src/pages/Bowls/Bowls.jsx
+++ src/pages/Bowls/Bowls.jsx
@@ -46,7 +46,7 @@ const nextSlide = () => {
                 return(
           
                 <div key={id} className="group overflow-hidden">
-                <img src={image} alt={title} onClick={()=> handleOpenModal(id)}  className="cursot-pointer h-42 w-72 object-cover"/>
+                <img src={image} alt={title} onClick={()=> handleOpenModal(id)}  className="cursor-pointer h-42 w-72 object-cover"/>
                 <div className="">
                 <h1 className="text-xl text-zinc-900 font-headings">{title}</h1>
                 <p className='text-sm text-zinc-600'>{description}</p>
