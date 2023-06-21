d7ad161f273e0f02456145c9a7f09d9f974e87ee
Sophie Perks <sophieperks@gmail.com>
2023-06-21 14:35:52 +0100

    paragraph layout to about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
-import aboutOne  from './assets/about.webp'
+import aboutOne from './assets/about.webp'
 
-function About (){
-    return(<>
+function About() {
+    return (<>
         <div className='mx-auto grid grid-cols-1 md:grid-flow-col grid-rows-2 text-center justify-items-center h-screen w-full '>
             <h2 className='text-2xl uppercase font-semibold font-headings tracking-widest pb-14 grid items-end justify-items-center'>About</h2>
-         <p className='mx-10'>I was born in Florence in 1970. I grew up in a family of artists in the countryside near Siena.
-I was drawn to clay, which my father worked in his studio from the 1980s. I moved to England to complete my education, and have been based in London since my twenties.
-In 2005 the ceramicist Betty Woodman introduced me to leaf printing in clay. Currently I work on smaller objects, like plates, as well as on the mural scale. </p>
-<img className='mr-10' src={aboutOne} alt="saskia spender" />
+            <p className='mx-10'>I was born in Florence in 1970. I grew up in a family of artists in the countryside near Siena.
+                I was drawn to clay, which my father worked in his studio from the 1980s. I moved to England to complete my education, and have been based in London since my twenties.
+                In 2005 the ceramicist Betty Woodman introduced me to leaf printing in clay. Currently I work on smaller objects, like plates, as well as on the mural scale. </p>
+            <img className='mr-10' src={aboutOne} alt="saskia spender" />
 
         </div>
-        <div className='mx-auto justify-center h-screen w-full'>
+        <div className='mx-auto w-full'>
             <h1 className='text-4xl text-neutral-800 flex uppercase font-bold pb-2 font-headings tracking-widest justify-center'>what to ask of ceramic art?</h1>
-            <h3 className='text-lg text-neutral-800 font-semibold font-headings tracking-widest pb-14 grid items-end justify-items-center'>Some reflections by Saskia Spender</h3>
+            <h3 className='text-lg text-neutral-800 font-semibold font-headings tracking-widest pb-14 flex items-end justify-center'>Some reflections by Saskia Spender</h3>
         </div>
-        </>
+        <section className='container mx-auto md:px-2 py-16 w-1/2'>
+            <div className='justify-center'>
+
+
+                <p className='text-sm text-justify pb-2'>I saw potters as a child in Tuscany. I used to be fascinated by the sight of clay rising like a genie between the potter’s hands, growing and shifting in an unmistakably phallic way, and then evolve into a feminine form: a cup, a vase, a bowl. A vessel.</p>
+                <br />
+                <p className='text-sm text-justify'>Who were these vessels? Were they contained by the potter’s hands, or were they the container? They would spend most of their lives empty. We could not live without them. Some we would come to treasure with use. Others, the least utilized, might be buried for thousands of years and become treasure in a local museum, for instance on a Cycladic island.</p>
+
+            </div>
+        </section>
+    </>
     )
 }
 
