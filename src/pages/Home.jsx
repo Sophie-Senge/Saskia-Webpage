3281802aa792f6f6ed0bc94aa47ae8131fc8e4af
Sophie Perks <sophieperks@gmail.com>
2023-06-21 13:21:20 +0100

    moved hero bg image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home (){
     return(
-        <div id="home" name="home" className=" bg-hero-pattern h-screen bg-no-repeat bg-cover bg-center bg-fixed">
+        <div id="home" name="home" className=" bg-hero-pattern h-screen bg-no-repeat bg-cover md:bg-[center_bottom_-14rem] bg-center bg-fixed">
 
             
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    // backgroundSize: {
+    //   'auto': 'auto',
+    //   'cover': '90%',
+    //   'contain': 'contain',
+    //   '50%': '50%',
+    //   '16': '4rem'
+    // },
     extend: {
       backgroundImage: {
         'hero-pattern': "url('./assets/images/landingPage.jpg')",
