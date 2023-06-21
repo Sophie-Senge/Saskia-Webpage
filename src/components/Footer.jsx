ffa5ed9db97ad7c87d13f442aa7c7e2ca9ed005c
Sophie Perks <sophieperks@gmail.com>
2023-06-21 16:27:55 +0100

    footer aligned correctly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 function Footer (){
     return(
-        <div className="w-full flex bg-white fixed bottom-0">
-        <div className=" mx-auto flex flex-row justify-center text-center py-4">
+        <div className="w-full h-16 flex bg-white fixed bottom-0">
+        <div className="mx-auto flex flex-row justify-center text-center">
         {/* <a className='mt-5 pr-4' href="https://www.instagram.com/saskiaspender/?hl=en"><SiInstagram size={15}/></a> */}
        
-           <a href="https://leliascarfiotti.com/"> <p className="mt-4 px-2 text-xs font-headings leading-6  text-black">
+           <a className='flex items-center justify-center mx-auto' href="https://leliascarfiotti.com/"> <p className="px-3 text-xs font-headings leading-6  text-black">
             photo©2020 lelia scarfiotto
             </p></a>
-            <a href="https://sophieperks.com/"><p className="mt-4 px-2 font-headings text-xs leading-6 text-black">
+            <a className='flex items-center justify-center mx-auto' href="https://sophieperks.com/"><p className="px-3 font-headings text-xs leading-6 text-black">
             built by SP©2023
             </p></a>
         </div>
