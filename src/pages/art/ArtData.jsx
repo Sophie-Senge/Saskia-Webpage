86426ebc6fc3bac8de69e7a10e499ab8ddd11e19
Sophie Perks <sophieperks@gmail.com>
2023-06-23 13:58:55 +0100

    added webp photos to art page

--- src/pages/art/ArtData.jsx
+++ src/pages/art/ArtData.jsx
@@ -2,49 +2,40 @@ const artList = [
     {
         id: 1,
         title: "Vases",
-        image: require("./artImages/100-9728.JPG"),
-        className: " absolute -my-44 mx-[94px] md:mx-[110px] opacity-0 group-hover:opacity-100",
+        image: require("./artImages/100-9728.webp"),
+        className: " absolute -my-32 mx-[94px] md:mx-[94px] opacity-0 group-hover:opacity-100",
         page: '/vases'
 
     },
     {
         id: 2,
         title: "Baskets",
-        image: require("./artImages/101-0194 copy.JPG"),
-        className: "absolute -my-44 mx-20 md:mx-24 opacity-0 group-hover:opacity-100",
+        image: require("./artImages/IMG_1206.webp"),
+        className: "absolute -my-32 mx-20 md:mx-24 opacity-0 group-hover:opacity-100",
         page: '/baskets'
 
     },
     {
         id: 3,
         title: "Bowls",
-        image: require("./artImages/100-9989.JPG"),
-        className: " absolute -my-44 mx-[96px] md:mx-[110px] opacity-0 group-hover:opacity-100",
+        image: require("./artImages/IMG_9946.webp"),
+        className: " absolute -my-32 mx-[96px] md:mx-[94px] opacity-0 group-hover:opacity-100",
         page: '/bowls'
 
     },
     {
         id: 4,
-        title: "Dreamscapes",
-        image: require("./artImages/IMG_0953.JPG"),
-        className: " absolute -my-44 mx-[52px] md:mx-[60px] opacity-0 group-hover:opacity-100",
+        title: "Tiles",
+        image: require("./artImages/IMG_1300.webp"),
+        className: " absolute -my-32 mx-[96px] md:mx-[94px] opacity-0 group-hover:opacity-100",
         page: '/dreamscapes'
 
     },
-    {
-        id: 5,
-        title: "Walls",
-        image: require("./artImages/IMG_0973.JPG"),
-        className: "absolute -my-44 mx-24 md:mx-28 opacity-0 group-hover:opacity-100",
-        page: '/walls'
-        
-
-    },
      {
-        id: 6,
+        id: 5,
         title: "Sculptures",
-        image: require("./artImages/101-0302.JPG"),
-        className: "absolute -my-44 mx-16 md:mx-20 opacity-0 group-hover:opacity-100",
+        image: require("./artImages/IMG_1260.webp"),
+        className: "absolute -my-32 mx-16 md:mx-16 opacity-0 group-hover:opacity-100",
         page: '/sculptures'
 
     },
