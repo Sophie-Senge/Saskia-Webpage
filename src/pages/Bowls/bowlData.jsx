068db8cbd7420b43c6b65ac141c12a04109e180e
Sophie Perks <sophieperks@gmail.com>
2023-06-26 15:11:58 +0100

    bowls compressed images

--- src/pages/Bowls/bowlData.jsx
+++ src/pages/Bowls/bowlData.jsx
@@ -2,58 +2,32 @@ const bowlList = [
     {
         id: 0,
         title: "Title",
-        image: require("./assets/101-0251.JPG"),
-        className: " absolute -my-44 mx-[94px] md:mx-[110px] opacity-0 group-hover:opacity-100",
-        page: '/vases',
+        image: require("./assets/bowl-1.webp"),
         description: 'Here will be description and measurements'
 
     },
     {
         id: 1,
         title: "Title",
-        image: require("./assets/101-0260.JPG"),
-        className: "absolute -my-44 mx-20 md:mx-24 opacity-0 group-hover:opacity-100",
-        page: '/baskets',
+        image: require("./assets/bowl-2.webp"),
         description: 'Here will be description and measurements'
 
     },
     {
         id: 2,
         title: "Title",
-        image: require("./assets/101-0360.JPG"),
-        className: " absolute -my-44 mx-[96px] md:mx-[110px] opacity-0 group-hover:opacity-100",
-        page: '/bowls',
+        image: require("./assets/bowl-3.webp"),
         description: 'Here will be description and measurements'
 
     },
     {
         id: 3,
         title: "Title",
-        image: require("./assets/101-0366.JPG"),
-        className: " absolute -my-44 mx-[52px] md:mx-[60px] opacity-0 group-hover:opacity-100",
-        page: '/dreamscapes',
+        image: require("./assets/bowl-4.webp"),
         description: 'Here will be description and measurements'
 
     },
-    {
-        id: 4,
-        title: "Title",
-        image: require("./assets/101-0482.JPG"),
-        className: "absolute -my-44 mx-24 md:mx-28 opacity-0 group-hover:opacity-100",
-        page: '/walls',
-        description: 'Here will be description and measurements'
-        
-
-    },
-     {
-        id: 5,
-        title: "Title",
-        image: require("./assets/101-0486.JPG"),
-        className: "absolute -my-44 mx-16 md:mx-20 opacity-0 group-hover:opacity-100",
-        page: '/sculptures',
-        description: 'Here will be description and measurements'
-
-    },
+ 
     
 
 ]
