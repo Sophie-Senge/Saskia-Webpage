c26833979c8d1655109096b5120ff895502eb6bd
Sophie Perks <sophieperks@gmail.com>
2023-06-26 17:21:39 +0100

    sculpture page done

---
+++ src/pages/Sculptures/SculptureData.jsx
@@ -0,0 +1,36 @@
+const sculptureList = [
+    {
+        id: 0,
+        title: "Title",
+        image: require("./assets/sculpture-1.webp"),
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 1,
+        title: "Title",
+        image: require("./assets/sculpture-2.webp"),
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 2,
+        title: "Title",
+        image: require("./assets/sculpture-3.webp"),
+        description: 'Here will be description and measurements'
+
+    },
+    {
+        id: 2,
+        title: "Title",
+        image: require("./assets/sculpture-4.webp"),
+        description: 'Here will be description and measurements'
+
+    },
+   
+ 
+     
+
+]
+
+export default sculptureList;
--- src/pages/Sculptures/Sculptures.jsx
+++ src/pages/Sculptures/Sculptures.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import sculptureList from './SculptureData';
 
-function Sculptures (){
-    return(
-        <div className='bg-slate-500 h-screen w-full'>
-            <p>Vases</p>
+import { AiOutlineRight, AiOutlineLeft, AiOutlineClose } from "react-icons/ai";
+
+function Sculptures() {
+
+
+  // modal below
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+  // console.log(slideNumber)
+  const handleOpenModal = (id) => {
+    setSlideNumber(id)
+    setOpenModal(true)
+    console.log(id)
+  }
+
+  const handleCloseModel = () => {
+    setOpenModal(false)
+  }
+
+  const prevSlide = () => {
+    slideNumber === 0 ? setSlideNumber(sculptureList.length - 1) : setSlideNumber(slideNumber - 1)
+  }
+
+  const nextSlide = () => {
+    slideNumber + 1 === sculptureList.length ? setSlideNumber(0) : setSlideNumber(slideNumber + 1)
+  }
+
+
+  return (
+    <div className="w-full h-max bg-white">
+
+      {openModal &&
+        <div className='fixed z-[999] flex justify-center items-center m-0 h-screen w-screen bg-neutral-900 bg-opacity-90'>
+          <div className="flex flex-row">
+            <AiOutlineClose className="text-white fixed z-[999] top-48 md:top-10 right-10 cursor-pointer" onClick={handleCloseModel} size={30} />
+            <AiOutlineLeft className="text-white fixed translate-y-[-25px] md:translate-y-[-50px] left-7 md:left-10 top-[50%] z-[999] cursor-pointer" onClick={prevSlide} size={30} />
+            <AiOutlineRight className="text-white fixed translate-y-[-25px] md:translate-y-[-50px] right-4 md:right-10 top-[50%] z-[999] cursor-pointer" onClick={nextSlide} size={30} />
+          </div>
+          <div className=" scale-75">
+            <img src={sculptureList[slideNumber].image} alt={sculptureList[slideNumber].title} />
+          </div>
+          <div className="fixed translate-y-[200px] md:translate-y-[327px] ">
+            <h1 className="text-slate-300 text-center">{sculptureList[slideNumber].title}</h1>
+            <p className="text-slate-300">{sculptureList[slideNumber].description}</p>
+          </div>
+
         </div>
-    )
+      }
+      <div className=" grid md:grid-rows-2 md:grid-cols-2 gap-10 grid-rows-1 justify-center items-center  text-center mb-28 mx-28 pt-40 ">
+        {sculptureList.map(({ id, title, image, description }) => {
+          return (
+
+            <div key={id} className="group overflow-hidden">
+              <img src={image} alt={title} onClick={() => handleOpenModal(id)} className="cursor-pointer aspect-[3/2] object-cover" />
+              <div className="">
+                <h1 className="text-xl text-zinc-900 font-headings">{title}</h1>
+                <p className='text-sm text-zinc-600'>{description}</p>
+              </div>
+            </div>
+
+          )
+
+        })}
+
+      </div>
+    </div>
+  )
 }
 
 export default Sculptures;
